d94a9923e19b2bf99a14087aea39312bba0e8ab7
vaishnavi Arikala <68643484+vaishnavinaidu@users.noreply.github.com>
2023-05-18 20:45:34 -0700

    Update logicgates.v

--- Day001_Logicgates/Behavioral_level/logicgates.v
+++ Day001_Logicgates/Behavioral_level/logicgates.v
@@ -1,13 +1,16 @@
-module logicgates (a,b,y1,y2,y3,y4,y5,y6,y7);
-  input a,b;
-  output y1, y2, y3, y4, y5, y6, y7;
-  
-  not (y1,a,b);
-  and (y2,a,b);
-  or (y3,a,b);
-  xor(y4,a,b);
-  xnor(y5,a,b);
-  nor(y6,a,b);
-  nand(y7,a,b);
-  
-  endmodule 
+module logicgates (a, b, y1, y2, y3, y4, y5, y6, y7);
+  input a, b;
+  output reg y1, y2, y3, y4, y5, y6, y7;
+
+always @(a or b)
+begin
+      y1 = a & b;
+      y2 = a | b; 
+      y3 = ~ a;
+      y4 = ~(a & b);
+      y5 = ~(a | b);
+      y6 = a ^ b;
+      y7 = ~(a ^ b);
+end
+
+endmodule
